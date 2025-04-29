88f5aa97fd45153f582ebc65b6c5b29cdc45e6f0
minyong2 <owow77@naver.com>
2025-04-30 01:39:00 +0900

    지그재그 숫자

---
+++ 1986.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+
+class Solution{
+	public static void main(String args[]) throws Exception{
+	
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++){
+			int num = sc.nextInt();
+            int sum = 0;
+            
+            for(int i=1; i<=num; i++){
+            	if(i % 2  == 0){ // 짝수
+                    sum -= i;
+                }else{ // 홀수
+                	sum += i;
+                }
+            }
+            System.out.println("#"+test_case+" "+sum); 
+		
+		}
+	}
+}
