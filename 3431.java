6606b53e78dc1a2397c07afc779d23e2a3143c14
minyong2 <owow77@naver.com>
2025-05-01 16:15:07 +0900

    D3 3431.준환이 운동

---
+++ 3431.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution{
+	public static void main(String args[]) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++){
+            int L = sc.nextInt();
+            int U = sc.nextInt();
+            int X = sc.nextInt();
+            int result = 0;
+            if(X < L){
+            	result = L-X;
+            }else if(X > U){
+            	result = -1;
+            }
+
+			System.out.println("#"+test_case+" "+result);   
+		}
+	}
+}
