697db5e487c44add221bf2f7b73e5762cdb758d5
minyong2 <owow77@naver.com>
2025-05-04 08:53:44 +0900

    D3 석찬이 받아쓰기

---
+++ day5/9317.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.FileInputStream;
+
+class Solution{
+	public static void main(String args[]) throws Exception{
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++){
+            int length = sc.nextInt();
+            sc.nextLine();
+            int cnt = 0;
+            
+            String s = sc.nextLine();
+            String answer = sc.nextLine();
+            
+            for(int j=0; j<length; j++){
+            	 if(s.charAt(j) == answer.charAt(j)){
+                	cnt ++;
+                }
+            }
+			System.out.println("#"+test_case+" "+cnt);  
+		}
+	}
+}
