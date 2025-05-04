c065ce19da6b3262bc728ec50c250e618e00e2bf
minyong2 <owow77@naver.com>
2025-05-04 09:12:30 +0900

    D3 Calkin-Wilf tree 1

---
+++ day5/11688.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.FileInputStream;
+
+class Solution{
+	public static void main(String args[]) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		sc.nextLine();
+		for(int test_case = 1; test_case <= T; test_case++){
+		/* L > a = a/a+b
+           R > b = a+b/b
+        */
+            int a = 1;
+        	int b = 1;
+            String  dir = sc.nextLine();
+            for(char s : dir.toCharArray()){
+            	if(s =='L'){
+            	b = a+b;
+            	}
+            	else if(s =='R'){
+            	a = a+b;
+            	}
+            }
+		System.out.println("#"+test_case+" "+a+" "+b); 
+		}
+	}
+}
